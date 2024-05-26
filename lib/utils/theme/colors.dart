ef864079a57ac51cfcc09dbd2c77eea5fb1a0bde
Krishnaprasad R <krishnaprasad.r@infinitylearn.com>
2024-05-26 18:20:17 +0530

    initial commit

---
+++ lib/utils/theme/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF0961F5);
+const Color backgroundColor = Color(0xFFFFFFFF);
+const Color componentsColor = Color(0xFF167F71);
+const Color overlayColor = Color(0xFFFFFFFF);
+
+// Font Colors
+
+const Color fontTitleColor = Color(0xFFFF6B00);
+const Color fontResultColor = Color(0xFF0961F5);
+const Color fontSecondaryColor = Color(0xFFA0A4AB);
